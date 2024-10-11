498c4c85010692faecafc06abf2fe5ff00f4c683
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-11 18:43:08 +0300

    what is the error

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,7 +5,7 @@ from typing import Any, Mapping, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """More involved type annotations"""
     if key in dct:
         return dct[key]
