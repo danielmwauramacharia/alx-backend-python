6436272d3fa659d08b1916632b66d4c638e09f6d
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-11 15:05:19 +0300

    Function that adds a mixed list and return float as an answer, annotations included

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+"""module: takes mixed list and return sum as float"""
+from typing import List, Union
+
+Mlist = List[Union[int, float]]
+
+
+def sum_mixed_list(mxd_lst: Mlist) -> float:
+    """Returns sum of a mixed lists as a float"""
+    return sum(mxd_lst)
