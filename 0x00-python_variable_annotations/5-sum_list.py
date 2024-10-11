57bc01102ff0338e16ff63961eab5219746632ff
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-11 14:58:10 +0300

    Function for summing a float list, with typing annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""module: sums a floating list"""
+from typing import List
+Lists = List[float]
+
+
+def sum_list(input_list: Lists) -> float:
+    """Return sum of a floating list"""
+    return sum(input_list)
