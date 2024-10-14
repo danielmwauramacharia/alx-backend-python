3940c697eaea3774c1527956aa6072fe8c842182
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-14 23:39:04 +0300

    remove the async in the function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 """Module: measure time"""
-from typing import List
 import time
 import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Measure the execution time of async function"""
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
