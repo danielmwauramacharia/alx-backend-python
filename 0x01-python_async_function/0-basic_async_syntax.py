0857fee89530278079d0ae47c1c48253732420ff
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-14 16:17:12 +0300

    adding changes

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """delay coroutine by the generated time"""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
