0f9c5d2b59123a741f7e401b76b0f786405a3b55
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-14 18:21:54 +0300

    typing annotation was the issue

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,11 +8,14 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawn an async function several times """
-    # result = []
-    # for _ in range(n):
-    #     output = await asyncio.gather(wait_random(max_delay))
-    #     result.extend(output)
-    # return result
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*tasks)
-    return sorted(results)
+    result = []
+    for _ in range(n):
+        output = await asyncio.gather(wait_random(max_delay))
+        result.extend(output)
+    return sorted(result)
+    # tasks = [wait_random(max_delay) for _ in range(n)]
+    # results = await asyncio.gather(*tasks)
+    # return sorted(results)
+# print(asyncio.run(wait_n(5, 5)))
+# print(asyncio.run(wait_n(10, 7)))
+# print(asyncio.run(wait_n(10, 0)))
