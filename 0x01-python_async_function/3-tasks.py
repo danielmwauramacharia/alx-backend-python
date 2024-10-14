bc0ee8b2d32777b9d41409c73b9fb6f78269b834
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-15 00:31:28 +0300

    Tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+##!/usr/bin/env python3
+"""Module: Return a Asyncio.Task"""
+from asyncio import Task, create_task
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Task:
+    """return a wrapper"""
+    output = create_task(wait_random(max_delay))
+    return output
