3943ced01701975a0fc194bcb57d5748596bbd11
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-14 18:06:16 +0300

    is it sorted?

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,4 @@
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 """module: working with multiple coroutines"""
 import asyncio
 
@@ -7,9 +7,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list:
     """spawn an async function several times """
-    result = []
-    for _ in range(n):
-        output = await asyncio.gather(wait_random(max_delay))
-        result.extend(output)
-    delays_ = sorted(result)
-    return delays_
+    # result = []
+    # for _ in range(n):
+    #     output = await asyncio.gather(wait_random(max_delay))
+    #     result.extend(output)
+    # return result
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
